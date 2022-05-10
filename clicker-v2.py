203bd7b5f500f6240840d12858dbe3c599002064
HamzaHammouti <hamzahhammouti@gmail.com>
2022-05-10 12:07:57 +0200

    Clicker V3 finished

--- clicker-v2.py
+++ clicker-v2.py
@@ -7,10 +7,12 @@ window.configure(bg="grey")
 
 number = 0
 kleur = 0
+status = ""
 
 def up():
-    global number, kleur
+    global number, kleur, status
     number += 1
+    status = "up"
     if number >= 1:
         window.configure(bg="green")
         kleur = 1
@@ -20,8 +22,9 @@ def up():
     button2["text"] = (number)
 
 def down():
-    global number, kleur
+    global number, kleur, status
     number -= 1
+    status = "down"
     if number <= -1:
         window.configure(bg="red")
         kleur = 2
@@ -41,6 +44,22 @@ def on_leave(e):
     elif kleur == 2:
         window.configure(bg="red")  
 
+def multiplier(e):
+    global status, number
+    if status == "up":
+        number = number * 3
+    elif status == "down":
+        number = number / 3
+    number = round(number,2)
+    button2.config(text=number)
+
+def update(self):
+
+    self.label.configure(text=self.time_string())
+
+    # schedule another timer
+    self.label.after(1000, self.update)
+
 button1 = tkinter.Button(window, text ="Up", width="30", command=up)
 button1.grid(row=0, column=0, padx=15, pady=15)
 button2 = tkinter.Label(window, text = number, width="30")
@@ -50,6 +69,7 @@ button3.grid(row=2, column=0, padx=15, pady=15)
 
 button2.bind("<Enter>", on_enter)
 button2.bind("<Leave>", on_leave)
+button2.bind("<Double-Button-1>", multiplier)
 
 # -------------------------------
 
